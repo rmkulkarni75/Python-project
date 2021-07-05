1223672fab3fb0b30c15e76d2d538051c21dadfb
rmkulkarni75 <79497518+rmkulkarni75@users.noreply.github.com>
2021-07-05 14:50:56 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,11 @@
+import random
+Number guessing game
+Guess a Number (between 1 and 9):
+Enter your guess:-3
+Your guess was to low: guess a number higher than 3
+Enter your guess:-4
+Your guess was to low: guess a number higher than 4
+Enter your guess:-7
+Your guess was to low: guess a number higher than 9
+Enter your guess:-9
+congratulation YOU WON
